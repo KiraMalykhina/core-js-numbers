88f9891ef83b31b90ca27721b2435560976bce73
[Malykhina Kira] <[kira4ka12@gmail.com]>
2023-11-17 01:54:54 +0200

    feat : add 5 - 10 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -85,8 +85,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  // throw new Error('Not implemented');
+  return -b / a;
 }
 
 /**
@@ -108,7 +109,20 @@ function getLinearEquationRoot(/* a, b */) {
  */
 function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
+  // return Math.atan2(y2, x2) - Math.atan2(y1, x1);
+  // const x = x2 - x1;
+  // const y = y2 - y1;
+  // if (y < 0 && x === 0) {
+  //   return Math.PI;
+  // } else if (y === 0 && x === 0) {
+  //   return 0;
+  // } else if (y > 0 && x < 0) {
+  //   return Math.PI / 2;
+  // } else if (y < 0 && x > 0) {
+  //   return Math.PI / 2;
+  // }
 }
+// console.log(getAngleBetweenVectors(0, -1, 1, 0));
 
 /**
  * Returns a last digit of a integer number.
@@ -123,8 +137,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -138,8 +152,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -155,8 +169,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -176,8 +190,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const square = 10 ** pow;
+  return Math.round(Math.floor(num) / square) * square;
 }
 
 /**
